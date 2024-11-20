8f80c55b45d57f3ee9154f9d25c9962e0f08bf61
bbegega <bernardo.begega@mobentis.com>
2024-11-20 13:30:17 +0100

    modulos creadis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { ClientsModule } from './clients/clients.module';
+import { FiltersModule } from './filters/filters.module';
+import { RejectsModule } from './rejects/rejects.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, ClientsModule, FiltersModule, RejectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
