eeccee48f481d84c13170fa9b2c5d5f6436c7064
bbegega <bernardo.begega@mobentis.com>
2025-02-04 09:28:53 +0100

    cofigurada conexxion a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,20 +14,32 @@ import { ConfigModule } from '@nestjs/config';
 import { CorsMiddleware } from './middleware/cors/cors.middleware';
 import { CompressionMiddleware } from './middleware/compression/compression.middleware';
 import { ImportModule } from './import/import.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,  // Esto hace que las variables de entorno estén disponibles globalmente
+  }),
+  TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      type: (process.env.DB_TYPE as 'mysql' | 'mssql' | 'postgres' | 'sqlite') || 'mysql', // Aquí se hace el casting
+      host: process.env.DB_HOST || '172.17.0.1',
+      port: parseInt(process.env.DB_PORT) || 3306, // MySQL por defecto
+      username: process.env.DB_USERNAME || 'mobentis',
+      password: process.env.DB_PASSWORD || 'msbs',
+      database: process.env.DB_NAME || 'db_rechazos',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false, // Usa 'false' en producción
+    }),
   }),
     ClientsModule, CompetitorsModule, FiltersModule, MenusModule, NavListsModule, ProductsModule, ReasonsRejectionModule, RejectsModule, UsersModule, ImportModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { 
+export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(CorsMiddleware, CompressionMiddleware)
-      .forRoutes('*'); 
+      .forRoutes('*');
   }
 }
--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
 export class LoginDto {
-    email: string;
-    password: string;
-  }
+  @IsNotEmpty({ message: 'El email es obligatoria' })
+  @IsEmail({}, { message: 'El email debe ser válido' })
+  email: string;
+
+  @IsNotEmpty({ message: 'La contraseña es obligatoria' })
+  @Matches(/^(?!.*['";\\%_]).*$/, { message: 'La contraseña contiene caracteres no permitidos' })
+  password: string;
+}
