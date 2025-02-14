6683a9834c10bde842a6efc85a071d3a5dc7beae
bbegega <bernardo.begega@mobentis.com>
2025-02-14 11:11:38 +0100

    modulo de navlist a falta de pruebas

--- src/nav-lists/repositories/nav-list.repository.ts
+++ src/nav-lists/repositories/nav-list.repository.ts
@@ -5,16 +5,13 @@ import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class NavListsRepository extends Repository<ListItem> {
-    getContainersByEntity(entity: string) {
-      throw new Error('Method not implemented.');
-    }
     
 
     constructor(@InjectRepository(ListItem) private readonly repo: Repository<ListItem>) {
         super(repo.target, repo.manager, repo.queryRunner);
     }
 
-    async getMenuItemsByIdAndLanguage(entity:string): Promise<ListItem[]> {
+    async getContainersByEntity(entity:string): Promise<ListItem[]> {
             const items = await this.repo.find({
               where: {
                 container_entity: entity,
